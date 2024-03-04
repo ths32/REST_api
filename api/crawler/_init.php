4bd889e694c6e83b8b452f6bfa00b1449a5b7054
ths32 <67457383+ths32@users.noreply.github.com>
2024-03-05 00:05:58 +0900

    Create _init.php

---
+++ api/crawler/_init.php
@@ -0,0 +1,12 @@
+<?php
+
+//define('ABS_PATH', dirname(__FILE__) . '/');
+
+$host = 'localhost';
+$username = '';
+$password = '';
+$database = '';
+
+//알림 연락처
+//$hp_array = ['01000000000',''];
+$hp_array = ['01000000000',''];
